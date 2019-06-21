4ba6fda34dedbc044967cf9f2b667d5d6a3bf1a7
andhikayuana <andhikayuana@gmail.com>
2019-06-21 19:15:08 +0700

    pertemuan 2

--- app/src/main/java/id/cashflow/android/MainActivity.kt
+++ app/src/main/java/id/cashflow/android/MainActivity.kt
@@ -1,8 +1,10 @@
 package id.cashflow.android
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import id.cashflow.android.util.Calculator
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * @author Yuana andhikayuana@gmail.com
@@ -13,6 +15,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ivNotification.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Notification", Toast.LENGTH_SHORT).show()
+        }
+        ivSetting.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Setting", Toast.LENGTH_SHORT).show()
+        }
 
         println("HALO ANDROID !")
 
