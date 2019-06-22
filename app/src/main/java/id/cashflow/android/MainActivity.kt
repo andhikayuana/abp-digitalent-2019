e4e9762d03c10d932ffb9f2e5c57aa6a0633df38
andhikayuana <andhikayuana@gmail.com>
2019-06-22 10:17:10 +0700

    pertemuan 2 menu popup

--- app/src/main/java/id/cashflow/android/MainActivity.kt
+++ app/src/main/java/id/cashflow/android/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import id.cashflow.android.util.Calculator
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,13 +20,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(toolbar)
-
         ivNotification.setOnClickListener {
             Toast.makeText(this@MainActivity, "Notification", Toast.LENGTH_SHORT).show()
         }
-        ivSetting.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Setting", Toast.LENGTH_SHORT).show()
+        ivSetting.setOnClickListener { view ->
+            val popup = PopupMenu(this@MainActivity, view)
+            val inflater: MenuInflater = popup.menuInflater
+            inflater.inflate(R.menu.menu_main_setting, popup.menu)
+            popup.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.action_setting -> {
+                        Toast.makeText(this@MainActivity, "Setting", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.action_logout -> {
+                        Toast.makeText(this@MainActivity, "Logout", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    else -> super.onOptionsItemSelected(it)
+                }
+            }
+            popup.show()
         }
 
         println("HALO ANDROID !")
